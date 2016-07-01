1041c143ee6da2f6ada6e8c7b3df57d679352488
Joshua Carroll <joshcarroll21@gmail.com>
2016-07-01 15:22:18 -0500

    Added GETDAILYRECORDS stored procedure
    Added a new SP.  The GETDAILYRECORDS stored procedure will return:
    - Rainfall since midnight
    - Low temp since midnight
    - High temp since midnight
    - Date/time when the most recent storm started
    - Date/time when the most recent storm ended
    - Total rainfall from the most recent storm
    
    For the “most recent storm” data, it simply finds the last record where
    rainfall amount is greater than zero - that is when the storm ended.
    It then finds the most recent record where rainfall amount equals zero
    and is before the storm end date/time.  Lastly it finds a sum of the
    rainfall amounts between those times.

--- CREATE-SP.sql
+++ CREATE-SP.sql
@@ -43,4 +43,24 @@ BEGIN
 	ORDER BY created DESC
 	LIMIT 1);
 END$$
-DELIMITER ;
+
+DELIMITER ##
+CREATE PROCEDURE `GETDAILYRECORDS`()
+BEGIN
+    SELECT @RainfallSinceMidnight:=SUM(RAINFALL) FROM WEATHER_MEASUREMENT WHERE created >= DATE(NOW());
+
+    SELECT @stormEnd:=created FROM WEATHER_MEASUREMENT WHERE rainfall > 0 ORDER BY created DESC LIMIT 1;
+    SELECT @stormStart:=created FROM WEATHER_MEASUREMENT WHERE rainfall = 0 AND created < @stormEnd ORDER BY created DESC LIMIT 1;
+    SELECT @stormTotal:=SUM(Rainfall) FROM WEATHER_MEASUREMENT WHERE created >= @stormStart AND created <= @stormEnd;
+
+    SELECT @LowSinceMidnight:= AMBIENT_TEMPERATURE FROM WEATHER_MEASUREMENT WHERE created >= DATE(NOW()) ORDER BY AMBIENT_TEMPERATURE ASC LIMIT 1;
+    SELECT @HighSinceMidnight:= AMBIENT_TEMPERATURE FROM WEATHER_MEASUREMENT WHERE created >= DATE(NOW()) ORDER BY AMBIENT_TEMPERATURE DESC LIMIT 1;
+
+    SELECT 
+        @RainFallSinceMidnight AS RainFallSinceMidnight,
+        @stormStart AS LastStormStart,
+        @stormEnd AS LastStormEnd,
+        @stormTotal AS LastStormTotal,
+        @LowSinceMidnight AS LowSinceMidnight,
+        @HighSinceMidnight AS HighSinceMidnight;
+END##

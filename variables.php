03436f533db9dc5f178b5c9f2c033367314c52df
Joshua Carroll <joshcarroll21@gmail.com>
2016-07-12 14:20:08 -0500

    Changed default measurements to imperial
    For testing purposes...  you see, I'm a very lazy dude and by committing
    this, it will be deployed to the demo server automatically.  So, while I
    could have made the change on the server in the time it took me to write
    this comment, I am lazy and a bit of a smart @$$.

--- variables.php
+++ variables.php
@@ -4,6 +4,6 @@ $databaseAddress = '127.0.0.1';
 $databaseUsername = 'root';
 $databasePassword = 'tiger';
 
-$useMetricAndCelsiusMeasurements = true;
+$useMetricAndCelsiusMeasurements = false;
 
 ?>

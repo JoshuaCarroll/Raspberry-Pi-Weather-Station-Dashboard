e98de2354bfadc52a58a205214fb32bd71717f72
Joshua Carroll <joshcarroll21@gmail.com>
2016-07-18 14:27:09 -0500

    Preferences now read from database...
    Need to change this so that these are stored in a session cookie.

--- variables.php
+++ variables.php
@@ -6,13 +6,19 @@ $databaseUsername = 'root';
 $databasePassword = 'tiger';
 $databaseSchema = 'weather';
 
-// ___ Preferences _____________________________________________________
-// These settings only change the display. Does not effect the values stored in the database.
+/* ____________________________________________________________________________
+*  
+*  It is suggested that you use the SETUP.sh script to change your preferences.
+*  See the README file for instructions on using the SETUP script.
+*
+*  Do not edit code below unless you understad the implications
+------------------------------------------------------------------------------ */
+set -- $(mysql -u root -p -N "select showMetricAndCelsiusMeasurements, showPressureInMillibars from RPiWx_SETTINGS")
 
 // If false displays values in Imperial units and Fahrenheit
-$showMetricAndCelsiusMeasurements = false; 
+$showMetricAndCelsiusMeasurements = "$1" 
 
 // If false displays pressure in inches (of Mercury)
-$showPressureInMillibars = true;
+$showPressureInMillibars = "$1"
 
 ?>

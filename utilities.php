49fcfd2cdae645e967ad56c249c3caeb68d47dad
Joshua Carroll <joshcarroll21@gmail.com>
2016-07-26 15:13:22 -0500

    Moved all utility functions into utilities.php, created Settings class

--- utilities.php
+++ utilities.php
@@ -1,7 +1,5 @@
 <?php
 
-// ______ Database functions _____________________________________________
-
 function getSetting($setting) {
     $con = new mysqli(DbSettings::$Address,DbSettings::$Username,DbSettings::$Password,DbSettings::$Schema);
     $returnValue = "";
@@ -22,14 +20,43 @@ function getSetting($setting) {
     return $returnValue;
 }
 
-// ------ General weather utilities ------------------------------------
 function calculateMeanSeaLevelPressure($pressure, $elevation) {
     $msl = $pressure / (pow(1 - ($elevation / 44330.0), 5.255));
     return $msl;
 }
 
-$showMetricAndCelsiusMeasurements = getSetting("showMetricAndCelsiusMeasurements");
-$showPressureInMillibars = getSetting("showPressureInMillibars");
-$stationElevationInMeters = getSetting("stationElevationInMeters");
+function convertKilometersToMiles($kilometers) {
+    $miles = $kilometers * 0.621371;
+    return $miles;
+}
+        
+function convertCelsiusToFahrenheit($celsiusDegrees) {
+    $F = ((($celsiusDegrees * 9) / 5) + 32);
+    return $F;
+}
+
+function convertMillibarsToInches($millibars) {
+    $inches = $millibars * 0.0295301;
+    return $inches;
+}
+
+function convertmillimetersToInches($mm) {
+    $inches = $mm * 0.039370;
+    return $inches;
+}
+
+function calculateDewPointF($tempC, $humidity) {
+    $dewPoint = $tempC - ((100 - $humidity) / 5);
+    $dewPoint = convertCelsiusToFahrenheit($dewPoint);
+    return $dewPoint;
+}
+
+// Database connection settings
+class Settings {
+    public static $showMetricAndCelsiusMeasurements = getSetting("showMetricAndCelsiusMeasurements");
+    public static $showPressureInMillibars = getSetting("showPressureInMillibars");
+    public static $stationElevationInMeters = getSetting("stationElevationInMeters");
+}
+
 
 ?>
